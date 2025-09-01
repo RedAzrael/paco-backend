a0e200b201422564574b207605207abfe516f3ea
Beast <beastofhate@protonmail.com>
2025-09-01 21:21:02 +0200

    Adapt for docker

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import mysql.connector
 from mysql.connector import Error
 from os import environ
